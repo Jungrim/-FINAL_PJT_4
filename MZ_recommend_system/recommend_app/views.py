e97fd3465f387139eeb3f4f7b80175a7f260baa3
Jungrim <sjr0930@gmail.com>
2022-10-25 09:36:50 +0900

    first commit

---
+++ MZ_recommend_system/recommend_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request, 'recommend_app/index.html')
+
+
+def trendSelect(request):
+    rating_traffic = request.POST['rate_1']
+    rating_safety = request.POST['rate_2']
+    rating_environment = request.POST['rate_3']
+    rating_facility = request.POST['rate_4']
+    rating_health = request.POST['rate_5']
+    print(rating_traffic,rating_safety ,rating_environment, rating_facility, rating_health)
+    return render(request, 'recommend_app/trendSelect.html')
