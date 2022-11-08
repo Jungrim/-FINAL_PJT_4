4bde1c96c0276a5ce8f421745a2137b33886984c
HG_laptop <dhsmfeh25@gmail.com>
2022-11-08 14:51:47 +0900

    please

--- MZ_recommend_system/recommend_app/urls.py
+++ MZ_recommend_system/recommend_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .import views
 
+app_name = 'recommend_app'
+
 urlpatterns = [
     path('', views.index),
     path('basicSelect', views.basicSelect),
     path('protoSubmit',views.protoSubmit),
-    path('categoryRanking',views.categoryRanking),
+    path('categoryRanking',views.categoryRanking, name='categoryRanking'),
 
 ]
