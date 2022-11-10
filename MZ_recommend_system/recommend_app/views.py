e55c58cc148bd948c7f88012748cd3c5d9b45368
cyless-hj <jhj6740@gmail.com>
2022-11-10 09:52:41 +0900

    [1110] Merge

--- MZ_recommend_system/recommend_app/views.py
+++ MZ_recommend_system/recommend_app/views.py
@@ -83,6 +83,7 @@ def dongDetail(request):
     data = {"dong_name" : request.POST['dong_name']}
     print(data)
     dong_name = request.POST['dong_name']
+    print(dong_name)
 
     dict_list = []
     cate_list = ['transportation', 'safety', 'noise_vibration_num', 'leisure_num', 'gym_num', 'golf_num', 'park_num', 'facilities', 'medical', 'starbucks_num', 'mc_num', 'vegan_cnt', 'coliving_num', 'education', 'parenting', 'kids_num', 'ani_hspt_num', 'safe_dlvr_num', 'car_shr_num', 'mz_pop_cnt']
