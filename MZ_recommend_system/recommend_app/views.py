8c9fdfae252e517d52ed309c4bdd6ca1a0075d3b
Jungrim <sjr0930@gmail.com>
2022-11-12 15:52:00 +0900

    chart.js 추가 작업 - views.py 충돌제거

--- MZ_recommend_system/recommend_app/views.py
+++ MZ_recommend_system/recommend_app/views.py
@@ -36,6 +36,8 @@ def basicSelect(request):
         user = [int(i) for i in list(req_dict.values())[1:-1]]
 
         df = RML.preprocessing_df()
+        graph_data = RML.minmax_scaling(df)
+
         basic_df, first_kmeans, first_pca = RML.first_clustering(df)
         first_category, second_category = RML.create_category(df)
         user_df, select = RML.user_scaling(first_category, second_category, user, df)
@@ -46,12 +48,13 @@ def basicSelect(request):
         recommend_dong_list = user_include_df.loc[result_dong_list]['DONG'].values
         recommend_gu_list = user_include_df.loc[result_dong_list]['GU'].values
         recommend_code_list = user_include_df.loc[result_dong_list].index.values
+        graph_data_list = graph_data.loc[result_dong_list].values.tolist()
         # result = {"dong": recommend_dong_list, "gu" : recommend_gu_list, "code" : recommend_code_list, "weight_user": user}
         result = zip(recommend_gu_list, recommend_dong_list, recommend_code_list)
 
         title, tags = RML.get_dong_cluster(result_dong_list[0])
 
-        return render(request, 'recommend_app/recommend_result.html', {'result': result,'sim_list':sim_list,'cluster_data' : {'title' : title,"tags" : tags}})
+        return render(request, 'recommend_app/recommend_result.html', {'result': result,'sim_list':sim_list,'cluster_data' : {'title' : title,"tags" : tags},'graph_data' : graph_data_list})
     else:
         form = WeightsForm()
 
