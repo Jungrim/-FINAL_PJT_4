3b948bd1ffc0420e7d93e1bb7dcf44902eef437f
Jungrim <sjr0930@gmail.com>
2022-11-14 15:11:12 +0900

    카테고리 탭 - 로그인시 이용가능하도록 변경

--- MZ_recommend_system/recommend_app/views.py
+++ MZ_recommend_system/recommend_app/views.py
@@ -65,6 +65,8 @@ def basicSelect(request):
 
 
 def categoryRanking(request):
+    if not request.user.is_authenticated:
+        return redirect('accounts/login')
     dict_list = []
     cate_list = ['transportation', 'safety', 'noise_vibration_num', 'leisure_num', 'gym_num', 'golf_num', 'park_num', 'facilities', 'medical', 'starbucks_num', 'mc_num', 'vegan_cnt', 'coliving_num', 'education', 'parenting', 'kids_num', 'ani_hspt_num', 'safe_dlvr_num', 'car_shr_num', 'mz_pop_cnt']
     
